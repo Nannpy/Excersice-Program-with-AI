7bed4251bdbddeff222f289259426dfa91d9f495
Nannpy <131631162+Nannpy@users.noreply.github.com>
2024-12-03 09:33:43 +0700

    Create lunge.py

---
+++ model/lunge.py
@@ -0,0 +1,87 @@
+import mediapipe as mp  
+import cv2
+import pydirectinput  
+import numpy as np
+
+mp_drawing = mp.solutions.drawing_utils   
+mp_pose = mp.solutions.pose
+
+def calculate_angle(a, b, c):
+    a = np.array(a)  # First point
+    b = np.array(b)  # Mid point 
+    c = np.array(c)  # End point
+    
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+    
+    if angle > 180.0:
+        angle = 360 - angle
+        
+    return angle
+
+cap = cv2.VideoCapture(0)
+
+lunge_counter = 0
+lunge_stage = None
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        results = pose.process(image)
+    
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        try:
+            landmarks = results.pose_landmarks.landmark
+
+            right_hip = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                         landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            right_knee = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                          landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            right_ankle = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                           landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+            
+            lunge_angle = calculate_angle(right_hip, right_knee, right_ankle)
+
+            cv2.putText(image, f'Lunge Angle: {lunge_angle:.2f}', 
+                           tuple(np.multiply(right_knee, [640, 480]).astype(int)), 
+                           cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+
+            if lunge_angle > 160:  
+                lunge_stage = "up"
+            if lunge_angle < 90 and lunge_stage == 'up':  
+                lunge_stage = "down"
+                lunge_counter += 1
+                print(f"Lunges completed: {lunge_counter}, pressing space")
+                pydirectinput.press('space')
+
+        except:
+            pass
+
+        cv2.rectangle(image, (0,0), (225,73), (245,117,16), -1)
+
+        cv2.putText(image, 'LUNGES', (15,12), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(lunge_counter), 
+                    (60,60), 
+                    cv2.FONT_HERSHEY_SIMPLEX, 2, (255,255,255), 2, cv2.LINE_AA)
+        
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2), 
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+        
+        cv2.imshow('Exercise Counter: Lunges', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
